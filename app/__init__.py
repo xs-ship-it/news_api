c1f3c684a6bcafc5830b181336062ebf0d84b1d7
Felix Nderitu <newtnfelix@gmail.com>
2021-11-05 13:22:04 +0300

    add a create app function

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    #Initializing application
+    app = Flask(__name__)
+
+    #Configuration
+    app.config.from_object(config_options[config_name])
+    
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+
+     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+     # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+
+    return app
