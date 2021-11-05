7836eb5b5bab0a9cb90840e2937e52cc6c172604
Felix Nderitu <newtnfelix@gmail.com>
2021-11-05 13:29:13 +0300

    add  article

--- app/models.py
+++ app/models.py
@@ -8,3 +8,17 @@ class Source:
         self.name = name
         self.description = description
         self.language = language
+
+class Article:
+    '''
+    Class Article to define article objects 
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
